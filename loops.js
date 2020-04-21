f09e06ec00f89a3ea2f8b10a3741308c515dec63
Robert Howden <robert.howden@current-rms.com>
2020-04-21 07:32:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 forLoop = (array) => {
   for (let i = 0; i < 25; i++) {
-    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+    array.push(`"I am ${i + 1} strange loop${i === 0 ? '' : 's'}."`)
   }
   return array
 }
